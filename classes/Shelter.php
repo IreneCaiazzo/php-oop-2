78f6eb26654f569f882a264b2a8d886018c5c8ba
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-26 15:39:21 +0200

    add variables

--- classes/Shelter.php
+++ classes/Shelter.php
@@ -3,4 +3,17 @@ include_once __DIR__ . '/Product.php';
 
 class Shelter extends Product
 {
+    public function __construct(
+        int $id,
+        string $name,
+        string $description,
+        int $price,
+        Category $category,
+        string $image,
+        private int $height,
+        private int $width,
+        private int $lenght,
+    ) {
+        parent::__construct($id, $name, $description, $price, $category, $image);
+    }
 }
--- classes/Toy.php
+++ classes/Toy.php
@@ -3,4 +3,16 @@ include_once __DIR__ . '/Product.php';
 
 class Toy extends Product
 {
+    public function __construct(
+        int $id,
+        string $name,
+        string $description,
+        int $price,
+        Category $category,
+        string $image,
+        protected string $material,
+        private string $color,
+    ) {
+        parent::__construct($id, $name, $description, $price, $category, $image);
+    }
 }
