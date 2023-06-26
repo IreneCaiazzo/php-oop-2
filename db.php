7b4caa294d2b9084e8c1f8bbdd4fa3fc81bde235
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-26 16:29:48 +0200

    add products in database

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+$arrProducts = [
+    new Food(1, 'Croccantini', 'Cibo buono', 1000, new Category(), 'url', '2023-08-30', 300),
+    new Toy(2, 'corda', 'Cibo buono', 1000, new Category(), 'url', 'stoffa', 'blu'),
+    new Shelter(3, 'Cuscino', 'Cibo buono', 1000, new Category(), 'url', 500, 700, 600),
+    new Toy(2, 'palla', 'Cibo buono', 1000, new Category(), 'url', 'plastica', 'rosso'),
+    new Food(1, 'scatoletta', 'Cibo buono', 1000, new Category(), 'url', '2023-08-30', 300),
+    new Shelter(3, 'Cuccetta', 'Cibo buono', 1000, new Category(), 'url', 500, 700, 600),
+];
