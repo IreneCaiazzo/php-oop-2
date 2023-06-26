7b97e64809c93c81a94e8a9b1c7fc11e26421422
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-26 15:30:11 +0200

    add include

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include_once __DIR__ . '/classes/Food.php';
+include_once __DIR__ . '/classes/Category.php';
+include_once __DIR__ . '/classes/Toy.php';
+include_once __DIR__ . '/classes/Shelter.php';
