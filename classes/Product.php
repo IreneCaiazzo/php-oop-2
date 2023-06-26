be38bfd0aebbb3d2496ec568b112050b17d62d5e
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-26 15:19:27 +0200

    add variables

--- classes/Product.php
+++ classes/Product.php
@@ -2,4 +2,13 @@
 
 class Product
 {
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $description,
+        private int $price,
+        private Category $category,
+        private string $image,
+    ) {
+    }
 }
